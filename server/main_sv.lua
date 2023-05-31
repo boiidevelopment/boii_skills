08c7ed2bd19b3b00cc05daa12dd0389edff2a3b2
Case <90377400+CaseIRL@users.noreply.github.com>
2023-05-31 20:03:42 +0100

    Update main_sv.lua

--- server/main_sv.lua
+++ server/main_sv.lua
@@ -10,16 +10,16 @@ if framework == 'boii' then
 elseif framework == 'qb' then
     fw = exports['qb-core']:GetCoreObject()
 elseif framework == 'esx' then
-    -- to do ...
+    fw = exports['es_extended']:getSharedObject()
 elseif framework == 'ox' then    
     -- to do...
 elseif framework == 'custom' then
-    -- to do...
+    -- add code for your own framework here
 end
 
 AddEventHandler('onResourceStart', function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then
       return
     end
-    print('^5--<^3!^5>-- ^7BOII ^5| ^7DEVELOPMENT ^5--<^3!^5>-- ^7UTILITY: SKILL SYSTEM V0.0.1 ^5--<^3!^5>--^7')
+    print('^5--<^3!^5>-- ^7BOII ^5| ^7DEVELOPMENT ^5--<^3!^5>-- ^7UTILITY: SKILL SYSTEM V0.0.2 ^5--<^3!^5>--^7')
 end)
